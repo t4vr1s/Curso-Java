e5fa0adb420af9cf8f805ac279bc22a0c4506ac1
ees <eescobar@computin.cl>
2021-06-29 23:04:53 -0400

    tarea 1

---
+++ src/tarea1/Main.java
@@ -0,0 +1,9 @@
+package tarea1;
+
+public class Main {
+    public static void main(String[] args) {
+        // ingresar una edad y nombre. comparar si es mayor(>18) de edad, menor(<18) o un adulto mayor(65)
+        // mostrar mensaje con nombre edad y en caso de ser
+        // adulto mostrar cuanto anios le faltan para ser un adulto mayor
+    }
+}
