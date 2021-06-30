f58262979e1242c769bec3273b5470835d42ea82
fabian collao <collao.fabian@gmail.com>
2021-06-29 22:51:15 -0400

    creando calculadora

--- src/exercises/Main.java
+++ src/exercises/Main.java
@@ -14,24 +14,52 @@ public class Main {
         int op; // variable declarada
 
 
+
         // ingresamos valores por la terminal
         System.out.println("ingrese un valor");
         num1 = sc.nextInt();
         System.out.println("ingrese segundo valor");
         num2 = sc.nextInt();
-
+        do{
         //menu
         System.out.println("Calculadora");
         System.out.println("1. Sumar");
+        System.out.println("2. RESTAR");
+        System.out.println("3. DIVISION");
+        System.out.println("4. MULTIPLICACION");
+        System.out.println("5. MODULO");
+        System.out.println("0. EXIT");
+
         op = sc.nextInt(); // inicializa el valor de op
 
-        switch (op){ //Variable 'op' might not have been initialized
-            case 1:
-                System.out.println("la suma de los valores ingresados es: " + (num1 + num2));
-                break;
-            default:
-                System.out.println("opcion incorrecta. escoja 1,2,3,4,5");
-                break;
-        }
+    switch (op){ //Variable 'op' might not have been initialized
+        case 1:
+            System.out.println("la suma de los valores ingresados es: " + (num1 + num2));
+            break;
+
+        case 2:
+            System.out.println("la resta de los valores ingresados es: " + (num1  - num2));
+            break;
+        case 3:
+            System.out.println(" la division de los valores es : " + (num1 / num2));
+            break;
+
+        case 4:
+            System.out.println(" la multiplicacion de los valores : "+ (num1* num2));
+            break;
+        case 5:
+            int suma=0;
+            suma= num1 + num2;
+            System.out.println("el residuo de  : " +  suma + " , entre 2 es:  "+ suma % 2);
+
+            break;
+
+        default:
+            System.out.println("USTED HA SALIDO SATISFACTORIAMENTE! ");
+            break;
+    }
+
+}while(op !=0 );
+
     }
 }
