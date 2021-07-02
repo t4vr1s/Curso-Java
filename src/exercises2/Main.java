3bf7a818c0b0d386b0b0dc405304b05a97a453c5
ees <eescobar@computin.cl>
2021-07-01 22:27:14 -0400

    tarea y ejercicio

---
+++ src/exercises2/Main.java
@@ -0,0 +1,10 @@
+package exercises2;
+
+public class Main {
+    public static void main(String[] args) {
+        // Obtener el descuento de un producto teniendo el nombre del producto, clave y precio original.
+        // El descuento lo hace en base a la clave. Si la clave es 01,
+        // El descuento es el 10% y si la clave es 02 el descuento es un 20%(sólo existen 2 claves)
+        // mostrar datos y descuentos si lo obtiene.
+    }
+}
---
+++ src/tarea2/Main.java
@@ -0,0 +1,10 @@
+package tarea2;
+
+public class Main {
+    // Se ingresa nombre y 3 notas de N alumnos. Mostrar por cada alumno el mensaje de acuerdo al promedio obtenido:
+    // Si promedio esta entre 1 y 3,9 imprimir datos y mensaje “Es mala nota.”
+    // Si promedio esta entre 4 y 5 imprimir datos y mensaje “Es suficiente.”
+    // Si promedio esta entre 5,1 y 6 imprimir datos y mensaje “Es buena nota.”
+    // Si promedio esta entre 6,1 y 7 imprimir datos y mensaje “Es muy buena nota.”
+
+}
